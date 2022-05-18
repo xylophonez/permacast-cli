35daad7859c66ac56d033a6d2c625d95196632a8
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-18 14:57:39 +0300

    feat: TerraSpace uploading hotfix

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,146 +1,164 @@
-import Arweave from 'arweave'
-import { sleep, SmartWeaveWebFactory } from 'redstone-smartweave';
-import { XMLParser } from 'fast-xml-parser';
-import * as fs from 'fs';
-import detectContentType from 'detect-content-type'
+import Arweave from "arweave";
+import axios from "axios";
+import parseString from "xml2js";
+import bufferToArrayBuffer from "buffer-to-arraybuffer";
+import { sleep, SmartWeaveWebFactory } from "redstone-smartweave";
+import { XMLParser } from "fast-xml-parser";
+import * as fs from "fs";
+import detectContentType from "detect-content-type";
 import { stripHtml } from "string-strip-html";
 
 const arweave = Arweave.init({
   host: "arweave.net",
   port: 443,
   protocol: "https",
+  timeout: 100000,
 });
 
-const CONTRACT_SRC = "KrMNSCljeT0sox8bengHf0Z8dxyE0vCTLEAOtkdrfjM"
+const CONTRACT_SRC = "KrMNSCljeT0sox8bengHf0Z8dxyE0vCTLEAOtkdrfjM";
 const NFT_SRC = "-xoIBH2TxLkVWo6XWAjdwXZmTbUH09_hPYD6itHFeZY";
 
 const smartweave = SmartWeaveWebFactory.memCached(arweave);
 const parser = new XMLParser();
-const RSS_URL = ''
-const showId = 'PX7NknKqPnK03ZX8BakhX3aLpGUA0JcauBtfBBqvivY'; // replace with an arg to the API call
-const podcastsEndpoint = 'https://permacast-cache.herokuapp.com/feeds/podcasts'
+const RSS_URL = "https://terraspaces.org/feed/podcast/";
+const SHOW_ID = "IKsjaUBJiKNDtLPIOyobkUM6iPtTKAK2bMDBu30KdmE"; // TERRASPACE.ORG PODCAST ID
+const podcastsEndpoint =
+  "https://whispering-retreat-94540.herokuapp.com/feeds/podcasts"; // PERMACAST CACHE NODE ENDPOINT
 
-const jwk = JSON.parse(	
-	fs.readFileSync('../secrets/jwk.json', {encoding: 'utf8'}) // replace with arconnect?
-);
+const jwk = JSON.parse("YOUR JWK");
 
 async function downloadEpisode(url) {
-  const res = await fetch(url);
-  const arrayBuffer = await res.arrayBuffer();
-  return arrayBuffer;
+
+  const res = (
+    await axios.get(url, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+
+  return bufferToArrayBuffer(res);
 }
 
 function sanitizeMime(fileType) {
-  if (fileType.includes('ogg')) {
-    return 'audio/ogg';
-  } else if (fileType.includes('mpeg')) {
-    return 'audio/mpeg';
-  } else if (fileType.includes('mp4')) {
-    return 'audio/mp4';
-  } else if (fileType.includes('wav')) {
-    return 'audio/wav';
-  } else if (fileType.includes('aiff')) {
-    return 'audio/aiff';
-  } else if (fileType.includes('flac')) {
-    return 'audio/flac';
+  if (fileType.includes("ogg")) {
+    return "audio/ogg";
+  } else if (fileType.includes("mpeg")) {
+    return "audio/mpeg";
+  } else if (fileType.includes("mp4")) {
+    return "audio/mp4";
+  } else if (fileType.includes("wav")) {
+    return "audio/wav";
+  } else if (fileType.includes("aiff")) {
+    return "audio/aiff";
+  } else if (fileType.includes("flac")) {
+    return "audio/flac";
+  } else if (fileType.includes("mp3")) {
+    return "audio/mp3";
   } else {
     return false;
   }
 }
 
 async function uploadEpisode(e, cover) {
+  const link = e.enclosure[0]["$"].url;
+  const fileType = e.enclosure[0]["$"].type;
+  const title = e.title[0];
+  const description = stripHtml(e.description[0]).result;
+  console.log({ link, fileType, title, description });
 
-  const link = e.link;
-  const title = e.title;
-  const description = stripHtml(e.description).result.substring(0, 247) + '...';
+  const wallet = await arweave.wallets.jwkToAddress(jwk);
+  console.log("\n\ndownloading a new episode...\n\n");
   const data = await downloadEpisode(link);
 
-  const fileType = sanitizeMime(detectContentType(Buffer.from(data)));
-  const wallet = await arweave.wallets.jwkToAddress(jwk);
-  
-  const tx = fileType && await arweave.createTransaction( {data: data}, jwk);
-      const initState = `{"issuer": "${wallet}","owner": "${wallet}","name": "${title}","ticker": "PANFT","description": '${description}',"thumbnail": "${cover}","balances": {"${wallet}": 1}}`;
-      tx.addTag("Content-Type", fileType);
-      tx.addTag("App-Name", "SmartWeaveContract");
-      tx.addTag("App-Version", "0.3.0");
-      tx.addTag("Contract-Src", NFT_SRC);
-      tx.addTag("Init-State", initState);
-      // Verto aNFT listing
-      tx.addTag("Exchange", "Verto");
-      tx.addTag("Action", "marketplace/create");
-      tx.addTag("Thumbnail", cover);
+  if (true) {
+    const tx = await arweave.createTransaction({ data: data }, jwk);
+    const initState = `{"issuer": "${wallet}","owner": "${wallet}","name": "${title}","ticker": "PANFT","description": 'episode from ${title}',"thumbnail": "${cover}","balances": {"${wallet}": 1}}`;
 
-      const audioTxId = await arweave.transactions.sign(tx, jwk);
-      const uploader = await arweave.transactions.getUploader(tx);
+    tx.addTag("Content-Type", "audio/mp3");
+    tx.addTag("App-Name", "SmartWeaveContract");
+    tx.addTag("App-Version", "0.3.0");
+    tx.addTag("Contract-Src", NFT_SRC);
+    tx.addTag("Init-State", initState);
+    // Verto aNFT listing
+    tx.addTag("Exchange", "Verto");
+    tx.addTag("Action", "marketplace/create");
+    tx.addTag("Thumbnail", cover);
 
-      while (!uploader.isComplete) {
-        await uploader.uploadChunk();
-        console.log(
-          `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
-        );
-      }
+    tx.reward = (+tx.reward * 3).toString();
 
-      console.log(audioTxId);
+    const audioTxId = await arweave.transactions.sign(tx, jwk);
+    const uploader = await arweave.transactions.getUploader(tx);
 
-      let epObj = {}
+    while (!uploader.isComplete) {
+      await uploader.uploadChunk();
+      console.log(
+        `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
+      );
+    }
+    console.log("\n\n\nEPISODE AUDIO TXID:");
+    console.log(tx.id);
 
-      epObj.name = title;
-      epObj.desc = description;
-      epObj.verto = false;
-      epObj.audio = tx.id;
-      epObj.type = fileType;
-      epObj.audioTxByteSize = data.byteLength;
-      await addShowtoState(epObj);
+    let epObj = {};
+
+    epObj.name = title;
+    epObj.desc = description;
+    epObj.verto = false;
+    epObj.audio = tx.id;
+    epObj.type = fileType;
+    epObj.audioTxByteSize = data.byteLength;
+    await addShowtoState(epObj);
+  }
 }
 
 async function getMetadata() {
-  const res = await fetch(podcastsEndpoint);
-  const p = await res.json();
+  const p = (await axios.get(podcastsEndpoint)).data;
+
   const podcasts = p.res;
-  let childOf;
-  let index;
-  let episodes;
-  
+  let childOf, index, episodes, pid, cover;
+
   for (let i = 0; i < podcasts.length; i++) {
-    if (podcasts[i].pid == showId) {
+    if (podcasts[i].pid == SHOW_ID) {
+      pid = podcasts[i].pid;
       childOf = podcasts[i].childOf;
       index = podcasts[i].index;
       episodes = podcasts[i].episodes;
+      cover = podcasts[i].cover;
     }
   }
-  return {childOf: childOf, index: index, episodes: episodes};
+  return { childOf, index, episodes, pid, cover };
 }
 
 const addShowtoState = async (show) => {
-
   const metadata = await getMetadata();
   const contractId = metadata.childOf;
-  const index = metadata.index;
+  const pid = metadata.pid;
 
   let input = {
-    'function': 'addEpisode',
-    'index': index,
-    'name': show.name,
-    'desc': show.desc,
-    'audio': show.audio,
-    'audioTxByteSize': show.audioTxByteSize,
-    'type': show.type
+    function: "addEpisode",
+    pid: pid,
+    name: show.name,
+    desc: show.desc,
+    content: show.audio,
+  };
+
+  let tags = {
+    Contract: contractId,
+    "App-Name": "SmartWeaveAction",
+    "App-Version": "0.3.0",
+    "Content-Type": "text/plain",
+    Input: JSON.stringify(input),
+  };
+
+  const interaction = await arweave.createTransaction({ data: show.desc });
+  for (const key in tags) {
+    interaction.addTag(key, tags[key]);
   }
 
-  console.log(input)
+  await arweave.transactions.sign(interaction, jwk);
+  await arweave.transactions.post(interaction);
 
-  let tags = { "Contract-Src": CONTRACT_SRC, "App-Name": "SmartWeaveAction", "App-Version": "0.3.0", "Content-Type": "text/plain" }
-  let contract = smartweave.contract(contractId).connect(jwk);
-  let txId = await contract.writeInteraction(input, tags);
-  console.log('addEpisode txid:');
-  console.log(txId)
-  if (show.verto) {
-    console.log('pushing to Verto')
-    await this.listEpisodeOnVerto(txId)
-  } else {
-    console.log('skipping Verto')
-  }
-}
+  console.log("\n\naddEpisode txid:");
+  console.log(interaction.id);
+};
 
 function parseTitles(uploadedEpisodes) {
   let titles = [];
@@ -150,23 +168,32 @@ function parseTitles(uploadedEpisodes) {
   return titles;
 }
 
+
 async function main() {
-  const response = await fetch(RSS_URL);
-  const rssXml = await response.text();
-  const rssJson = parser.parse(rssXml);
-  const cover = rssJson.rss.channel.image.url;
-  const episodes = rssJson.rss.channel.item.reverse();
-  const metadata = getMetadata();
-  const uploadedEpisodes = ((await metadata).episodes);
+  let rssJson;
+  const rssXml = (await axios.get(RSS_URL)).data;
+  const json = parseString.parseString(
+    rssXml,
+    (err, result) => (rssJson = result.rss)
+  );
+
+  const cover = rssJson.channel[0]["itunes:image"];
+  const episodes = rssJson.channel[0].item;
+
+  const metadata = await getMetadata();
+
+  const uploadedEpisodes = (await metadata).episodes;
   const existingTitles = parseTitles(uploadedEpisodes);
 
   for (let i = 0; i < episodes.length; i++) {
+    // console.log(episodes[i])
     let e = episodes[i];
-    ! existingTitles.includes(e.title) && await uploadEpisode(e, cover);
-    sleep(10000)
-    console.log('sleep ended');
-  }
 
+    if (!existingTitles.includes(e.title[0])) {
+      await uploadEpisode(e, metadata.cover);
+      // console.log('sleep ended');
+    }
+  }
 }
 
-main()
+main();
